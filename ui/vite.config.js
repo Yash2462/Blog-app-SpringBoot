8d139c460f71cb24490e085d9295caef1149b24c
Yash2462 <patelyash2462@gmail.com>
2026-04-14 14:14:11 +0530

    port configured

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  }
 })
